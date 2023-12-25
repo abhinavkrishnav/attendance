ed5f743f9e8153b5d144b6cb913f3f47ba174bf7
abhinav krishna v <96730888+abhinavkrishnav@users.noreply.github.com>
2023-12-25 21:23:39 +0530

    rollback1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Attendancepage.css'; // Import the CSS file for styling
+const AttendancePage = () => {
+  const [attendance, setAttendance] = useState(Array(28).fill(true));
+  const toggleAttendance = (index) => {
+    setAttendance((prevAttendance) => {
+      const newAttendance = [...prevAttendance];
+      newAttendance[index] = !newAttendance[index];
+      return newAttendance;
+    });
+  };
 
-const StylishHeadingBox = () => {
   return (
-    <div className="heading-box">
-      Your Heading
-    </div>
-  );
-};
+    <div className="attendance-container">
 
-export default StylishHeadingBox;
+        <h1 className="header">SUBSTITUTION</h1>
+
+      <div className="attendance-box">
+        <h1>Attendance</h1>
+        <table>
+          <thead>
+            <tr>
+              <th>Serial Number</th>
+              <th>Name</th>
+              <th>Attendance</th>
+            </tr>
+          </thead>
+          <tbody>
+            {Array.from({ length: 28 }, (_, index) => (
+              <tr key={index}>
+                <td>{index + 1}</td>
+                <td>{`Teacher ${index + 1}`}</td>
+                <td>
+                  <input
+                    type="checkbox"
+                    checked={attendance[index]}
+                    onChange={() => toggleAttendance(index)}
+                  />
+                </td>
+              </tr>
+              ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      );
+    };
+    export default AttendancePage;
