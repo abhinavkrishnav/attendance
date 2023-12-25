c6c7ad7c48012015c37d770d4f759f87d26b0aae
abhinav krishna v <96730888+abhinavkrishnav@users.noreply.github.com>
2023-12-25 23:02:00 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React, { useState } from 'react';
-import './Attendancepage.css'; // Import the CSS file for styling
+import './Attendancepage.css'; // Import the CSS file
+
 const AttendancePage = () => {
+  // State to manage the checkbox status for each teacher
   const [attendance, setAttendance] = useState(Array(28).fill(true));
+
+  // Function to toggle the checkbox status for a teacher
   const toggleAttendance = (index) => {
     setAttendance((prevAttendance) => {
       const newAttendance = [...prevAttendance];
@@ -11,38 +15,34 @@ const AttendancePage = () => {
   };
 
   return (
-    <div className="attendance-container">
-
-        <h1 className="header">SUBSTITUTION</h1>
-
-      <div className="attendance-box">
-        <h1>Attendance</h1>
-        <table>
-          <thead>
-            <tr>
-              <th>Serial Number</th>
-              <th>Name</th>
-              <th>Attendance</th>
+    <div>
+      <h1>Attendance</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Serial Number</th>
+            <th>Teacher Name</th>
+            <th>Attendance</th>
+          </tr>
+        </thead>
+        <tbody>
+          {Array.from({ length: 28 }, (_, index) => (
+            <tr key={index}>
+              <td>{index + 1}</td>
+              <td>{`Teacher ${index + 1}`}</td>
+              <td>
+                <input
+                  type="checkbox"
+                  checked={attendance[index]}
+                  onChange={() => toggleAttendance(index)}
+                />
+              </td>
             </tr>
-          </thead>
-          <tbody>
-            {Array.from({ length: 28 }, (_, index) => (
-              <tr key={index}>
-                <td>{index + 1}</td>
-                <td>{`Teacher ${index + 1}`}</td>
-                <td>
-                  <input
-                    type="checkbox"
-                    checked={attendance[index]}
-                    onChange={() => toggleAttendance(index)}
-                  />
-                </td>
-              </tr>
-              ))}
-              </tbody>
-            </table>
-          </div>
-        </div>
-      );
-    };
-    export default AttendancePage;
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default AttendancePage;
