e1e58baf8f32010473dfbecdebe3461b748a8035
abhinav krishna v <96730888+abhinavkrishnav@users.noreply.github.com>
2023-12-25 21:16:44 +0530

    hovering

--- src/App.js
+++ src/App.js
@@ -1,50 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './Attendancepage.css'; // Import the CSS file for styling
 
-const AttendancePage = () => {
-  const [attendance, setAttendance] = useState(Array(28).fill(true));
-
-  const toggleAttendance = (index) => {
-    setAttendance((prevAttendance) => {
-      const newAttendance = [...prevAttendance];
-      newAttendance[index] = !newAttendance[index];
-      return newAttendance;
-    });
-  };
-
+const StylishHeadingBox = () => {
   return (
-    <div className="attendance-container">
-      
-        <h1 className="header">SUBSTITUTION</h1>
-        
-      <div className="attendance-box">
-        <h1>Attendance</h1>
-        <table>
-          <thead>
-            <tr>
-              <th>Serial Number</th>
-              <th>Name</th>
-              <th>Attendance</th>
-            </tr>
-          </thead>
-          <tbody>
-            {Array.from({ length: 28 }, (_, index) => (
-              <tr key={index}>
-                <td>{index + 1}</td>
-                <td>{`Teacher ${index + 1}`}</td>
-                <td>
-                  <input
-                    type="checkbox"
-                    checked={attendance[index]}
-                    onChange={() => toggleAttendance(index)}
-                  />
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+    <div className="heading-box">
+      Your Heading
     </div>
   );
 };
-export default AttendancePage;
+
+export default StylishHeadingBox;
